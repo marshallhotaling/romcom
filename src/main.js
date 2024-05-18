cc438e1409b553fddc2de94dc7e373d65bd95b51
Charles B. <charlesbanks238@gmail.com>
2024-05-18 15:33:51 -0700

    Cleans up the JS file and orders the functions based on iteration implementation.
    Co-Authored By: MARSHALL marshall.hotaling@gmail.com
    Co-Authored By: CHARLES charlesbanks238@gmail.com

--- src/main.js
+++ src/main.js
@@ -36,42 +36,18 @@ var saveThis = document.querySelector(".save-cover-button")
 
 var savedCoversSection = document.querySelector(".saved-covers-section")
 
-
-// var miniCoverCont = document.querySelector(".mini-cover");
-// var miniCoverImg = document.querySelector("#mini-cover-img");
-// var miniCoverTitle = document.querySelector("#mini-cover-title");
-// var miniCoverTagline = document.querySelector("#mini-cover-tagline");
-// var miniCoverPrice = document.querySelector("#mini-price-tag");
-// var miniCoverOverlay = document.querySelector("#mini-overlay");
-
 // We've provided a few variables below
-
-// var savedCovers = [
-//   createCover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
-// ];
 
 var savedCovers = [];
 var currentCover = createCover(picture1, mainTitle, D1, D2);
 
 // Add your event listeners here 👇
-// randomCover.addEventListener('click', () => {
-//   console.log("here")
-//   picture.src = covers[Math.floor(Math.random() * covers.length)]
-//   mainTitle.innerText = titles[Math.floor(Math.random() * titles.length)]
-//   D1.innerText = descriptors[Math.floor(Math.random() * descriptors.length)]
-//   D2.innerText = descriptors[Math.floor(Math.random() * descriptors.length)]
-// });
 
 makeACoverButton.addEventListener('click', showFormPage, console.log('1 <><'))
 homeButton.addEventListener('click', showMainPage, console.log('3 <<<><'))
 savedCoversButton.addEventListener('click', showSavedCovers, console.log('6 <<<<<<><'))
 
-
-
 //~~~~~~~~~~~~~~~~~~~~~~~~~ ITERATION 0 ~~~~~~~~~~~~~~~~~~~~~~~~~
-
-// document.addEventListener("DOMContentLoaded", randomCover(picture1, title, D1, D2))
-randomCoverButton.addEventListener("click", randomCoverHandler, console.log("62 ><>"))
 
 randomCoverButton.addEventListener('click', function () {
     console.log("here")
@@ -91,12 +67,8 @@ savedCoversButton.addEventListener('click', showSavedCovers)
 
 makeMyBookButton.addEventListener('click', createUserCover)
 
-
-
-
 //~~~~~~~~~~~~~~~~~~~~~~~~~~ ITERATION 3 ~~~~~~~~~~~~~~~~~~~~~~~~~
-saveThis.addEventListener('click', saveACover)
-savedCoversButton.addEventListener('click', displaySavedCovers);
+saveThis.addEventListener('click', saveACover);
 
 // Create your event handlers and other functions here 👇
 
@@ -105,52 +77,19 @@ savedCoversButton.addEventListener('click', displaySavedCovers);
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~ ITERATION 0 ~~~~~~~~~~~~~~~~~~~~~~~~~
 
-function randomCoverHandler () {
-    makeRandomCover();
-    displayRandomCover();
-};
-
-function userCover () {};
-
 function getRandomIndex(array) {
     return Math.floor(Math.random() * array.length);
 }
 
 function createCover(imgSrc, title, descriptor1, descriptor2) {
-    var cover = {
-        id: Date.now(),
-        coverImg: imgSrc,
-        title: title,
-        tagline1: descriptor1,
-        tagline2: descriptor2,
-        from: "createCover => currentCover => saveCover => savedCovers"
-    }
-    return cover
-}
-
-function makeRandomCover () {
-    randPic = covers.at(getRandomIndex(covers));
-    randTitle = titles.at(getRandomIndex(titles));
-    randD1 = descriptors.at(getRandomIndex(descriptors));
-    randD2 = descriptors.at(getRandomIndex(descriptors));
-    var randomCover = {
-        id: Date.now(),
-        coverImg: randPic,
-        title: randTitle,
-        tagline1: randD1,
-        tagline2: randD2,
-        from: "random func"
-    }
-    // console.log("129 ><>")
-    return randomCover
-}
-
-function displayRandomCover () {
-    var randomCover = makeRandomCover();
-    picture1.src = randomCover.coverImg;
-    mainTitle.innerText = randomCover.title;
-    D1.innerText = randomCover.tagline1;
-    D2.innerText = randomCover.tagline2;
+  var cover = {
+      id: Date.now(),
+      coverImg: imgSrc,
+      title: title,
+      tagline1: descriptor1,
+      tagline2: descriptor2,
+  }
+  return cover
 }
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~ ITERATION 1 ~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -174,41 +113,19 @@ function showMainPage() {
     saveButton.classList.remove('hidden');
 }
 
-function deleteItem(index) {
-    console.log('index', index)
-    savedCovers.splice(index, 1)
-    savedCoversSection.innerHTML = "";
-    for(let i = 0; i < savedCovers.length; ++i) {
-        savedCoversSection.innerHTML += displayItem(savedCovers[i].coverImg, savedCovers[i].title, savedCovers[i].tagline1, savedCovers[i].tagline2, i)
-    }
-}
-
 function showSavedCovers () {
-    savedCoversPage.classList.remove('hidden');
-    mainPage.classList.add('hidden');
-    formPage.classList.add('hidden');
-    homeButton.classList.remove('hidden');
-    randomCoverButton.classList.add('hidden');
-    saveButton.classList.add('hidden');
-    savedCoversSection.innerHTML = "";
+  savedCoversPage.classList.remove('hidden');
+  mainPage.classList.add('hidden');
+  formPage.classList.add('hidden');
+  homeButton.classList.remove('hidden');
+  randomCoverButton.classList.add('hidden');
+  saveButton.classList.add('hidden');
+  savedCoversSection.innerHTML = "";
 
-    for(let i = 0; i < savedCovers.length; ++i) {
-        savedCoversSection.innerHTML += displayItem(savedCovers[i].coverImg, savedCovers[i].title, savedCovers[i].tagline1, savedCovers[i].tagline2, i)
-    }
+  for(let i = 0; i < savedCovers.length; ++i) {
+      savedCoversSection.innerHTML += displayItem(savedCovers[i].coverImg, savedCovers[i].title, savedCovers[i].tagline1, savedCovers[i].tagline2, i)
+  }
 }
-
-function displayItem(coverImg, title, tag1, tag2, i) {
-    return `<article class="mini-cover" id="mini-cover-container" ondblclick="deleteItem(${i})">
-      <img class="mini-cover" src="${coverImg}" id="mini-cover-img">
-      <h2 class="cover-title" id="mini-cover-title">${title}</h2>
-      <h3 class="tagline" id="mini-cover-tagline">A tale of <span class="tagline-1">${tag1}</span> and
-       <span class="tagline-2">${tag2}</span></h3>
-      <img class="price-tag" id="mini-price-tag" src="./assets/price.png">
-      <img class="overlay" id="mini-overlay" src="./assets/overlay.png">
-    </article>`
-}
-
-
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~ ITERATION 2 ~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -225,65 +142,41 @@ function createUserCover (event) {
     descriptors.push(userD2.value);
 }
 
-
 //~~~~~~~~~~~~~~~~~~~~~~~~~ ITERATION 3 ~~~~~~~~~~~~~~~~~~~~~~~~~
 
 function saveACover () {
-    var hasItem = false
-    var coverToKeep = createCover(picture1.src, mainTitle.innerHTML, D1.innerHTML, D2.innerHTML);
+  var hasItem = false
+  var coverToKeep = createCover(picture1.src, mainTitle.innerHTML, D1.innerHTML, D2.innerHTML);
 
-    for (i = 0; i < savedCovers.length; i++) {
-        if (savedCovers[i].coverImg === coverToKeep.coverImg && savedCovers[i].mainTitle === coverToKeep.mainTitle && savedCovers[i].D1 === coverToKeep.D1 && savedCovers[i].D2 === coverToKeep.D2) {
-            hasItem = true
-        }
-    }
+  for (i = 0; i < savedCovers.length; i++) {
+      if (savedCovers[i].coverImg === coverToKeep.coverImg && savedCovers[i].mainTitle === coverToKeep.mainTitle && savedCovers[i].D1 === coverToKeep.D1 && savedCovers[i].D2 === coverToKeep.D2) {
+          hasItem = true
+      }
+  }
 
-    if (hasItem === false) {
-        savedCovers.push(coverToKeep)
-    }
-    console.log('savedCovers', savedCovers)
+  if (hasItem === false) {
+      savedCovers.push(coverToKeep)
+  }
 }
 
-// function saveACover () {
-//   var coverToKeep = createCover(userCoverPic.value, userTitle.value, userD1.value, userD2.value);
-//   for (i = 0; i < savedCovers.length; i++) {
-//     if (coverToKeep === savedCovers[i] || coverToKeep.title === "") {
-//       // console.log("1 <><", savedCovers);
-//       return savedCovers;
-//     } else if (coverToKeep !== savedCovers[i] || coverToKeep.title !== "") {
-//       // console.log("2 <<><", savedCovers);
-//       savedCovers.push(coverToKeep)
-//     }
-//   }
-// }
-
-function displaySavedCovers () {
-    console.log("empty func ><>")
+function displayItem(coverImg, title, tag1, tag2, i) {
+  return `<article class="mini-cover" id="mini-cover-container" ondblclick="deleteItem(${i})">
+    <img class="mini-cover" src="${coverImg}" id="mini-cover-img">
+    <h2 class="cover-title" id="mini-cover-title">${title}</h2>
+    <h3 class="tagline" id="mini-cover-tagline">A tale of <span class="tagline-1">${tag1}</span> and
+     <span class="tagline-2">${tag2}</span></h3>
+    <img class="price-tag" id="mini-price-tag" src="./assets/price.png">
+    <img class="overlay" id="mini-overlay" src="./assets/overlay.png">
+  </article>`
 }
 
-// function displaySavedCovers () {
-//   savedCoversSection.innerHTML = "";
-//   for (var i = 0; i < savedCovers.length; i++) {
-//     var cover = savedCovers[i];
-//     var miniCoverContainer = document.createElement("div");
-//       miniCoverContainer.className = "mini-cover";
+//~~~~~~~~~~~~~~~~~~~~~~~~~ ITERATION 4 ~~~~~~~~~~~~~~~~~~~~~~~~~
 
-//     var miniCoverImg = document.createElement("img");
-//       miniCoverImg.className = ".mini-cover img";
-//       miniCoverImg.setAttribute("id", "cover-image");
-//       // miniCoverImg.getElementById("cover-image").style = miniCoverContainer.style;
-//       miniCoverImg.src = picture.src;
-//       miniCoverContainer.appendChild(miniCoverImg);
-
-//     var miniCoverTitle = document.createElement("h2");
-//       miniCoverTitle.className = ".cover-title";
-//       miniCoverTitle.innerText = `${mainTitle.innerText}`;
-//       miniCoverContainer.appendChild(miniCoverTitle);
-
-//     var miniCoverTags = document.createElement("h3");
-//       miniCoverTags.className = ".tagline";
-
-
-//     savedCoversSection.appendChild(miniCoverContainer);
-//   }
-// }
+function deleteItem(index) {
+  console.log('index', index)
+  savedCovers.splice(index, 1)
+  savedCoversSection.innerHTML = "";
+  for(let i = 0; i < savedCovers.length; ++i) {
+      savedCoversSection.innerHTML += displayItem(savedCovers[i].coverImg, savedCovers[i].title, savedCovers[i].tagline1, savedCovers[i].tagline2, i)
+  }
+}
